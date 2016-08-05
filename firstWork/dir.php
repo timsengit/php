400aa820310138170c2f6e140094ee088042e111
tim <1547681531@qq.com>
2016-08-05 11:47:38 +0800

    整理提交文件夹，将第一次提交的作业置入firstWork中

---
+++ firstWork/dir.php
@@ -0,0 +1,33 @@
+﻿<html>
+<head>
+<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+<title>递归遍历指定文件夹：</title>
+</head>
+<body>
+
+<?php
+header("Content-Type: text/html;charset=utf-8");
+function listDir($dir)
+{
+    if (is_dir($dir)) {
+        if ($dh = opendir($dir)) {
+            while (($file = readdir($dh)) !== false) {
+                if ((is_dir($dir . "/" . $file)) && $file != "." && $file != "..") {
+                    echo "<font color='red'>文件名：</font>", $file, "<br><hr>";
+                    listDir($dir . "/" . $file . "/");
+                } else {
+                    if ($file != "." && $file != "..") {
+                        echo $file . "<br>";
+                    }
+                }
+            }
+            closedir($dh);
+        }
+    }
+}
+
+listDir("./");
+
+?>
+</body>
+</html>
---
+++ firstWork/showsort.php
@@ -0,0 +1,43 @@
+﻿<html>
+<head>
+<title>排序算法演示：</title>
+</head>
+<body>
+
+<?php
+
+require_once("./sortes.php");
+
+
+$arr=array(4,3,1,9,2,7,5,8,6);
+show($arr);
+
+echo "</br>插入排序：";
+$arrinsert=insert_sort($arr);
+show($arrinsert);
+
+echo "</br>冒泡排序：";
+$arrbubble=bubble_sort($arr);
+show($arrbubble);
+
+echo "</br>选择排序：";
+$arrselect=select_sort($arr);
+show($arrselect);
+//echo `dir c:`;
+echo "</br>快速排序：";
+$arrquick=quick_sort($arr);
+show($arrquick);
+echo "</br>希尔排序：";
+$arrshell=shell_sort($arr);
+show($arrshell);
+
+function show($arr3)
+{
+	for ($i=0; $i <count($arr3) ; $i++) { 
+		# code...
+		echo " ".$arr3[$i];
+	}
+}
+?>
+</body>
+</html>
