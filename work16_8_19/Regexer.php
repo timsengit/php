31e99763c0b8406bfed991b69f390d056c96275e
tim <1547681531@qq.com>
2016-08-29 09:25:08 +0800

    正则作业

--- work16_8_19/Regexer.php
+++ work16_8_19/Regexer.php
@@ -10,10 +10,45 @@ namespace Regex;
 
 class Regexer
 {
-
-    public function preg($pattern, $subject, &$matches)
+    public function regexerArray($arr)
     {
-        $str1 = '';
-        return preg_match($pattern, $subject, $matches);
+        /*
+         *使用正则表达式找出数组中以字母开始和以数字结束，并且没有空格的单元
+         */
+        foreach ($arr as $row) {
+            if (preg_match('/^[a-zA-Z]\S*[0-9]$/', $row)) {
+                $str[] = $row;
+            }
+        }
+        return $str;
+    }
+    public function regexerHttp($http)
+    {
+        /*
+         * 使用正则分解http
+         *
+         */
+        $re = '@^(http://)+(\w+.)(\w+.)(\w+.)(\w.*)@i';
+        if (preg_match($re, $http, $date)) {
+            $str[] = $date;
+        }
+        return $str;
+    }
+    public function takeInte($text)
+    {
+        /*
+         *正则取出上面字符串里的所有网址
+         */
+        preg_match_all('/<a\s.*?href=[\'|\"]([^\"\']*)[\'|\"][^>]*>/is', $text, $matched);
+        return $matched[1];
+    }
+    public function deleteHtml($text)
+    {
+        /*
+         *通过替换的方式除掉所有html标签
+         */
+        $delect = '/<\/?[^>]+>/';
+        $arr    = preg_replace($delect, '', $text); //直接返回
+        return $arr;
     }
 }
--- work16_8_19/tester.php
+++ work16_8_19/tester.php
@@ -6,22 +6,16 @@
  * Time: 13:42
  */
 include "./Regexer.php";
-$regex   = new \Regex\Regexer();
-$subject = 'foobar: 2008';
-$pattern = '/\bfooba/';
-//echo $regex->preg($pattern, $subject, $matches);
-//echo $matches[0];
-//var_dump($matches);
-//1
-//2
-//3
-//从URL中获取主机名称
-$regex->preg('@^(?:http://)?([^/]+)@i',
-    "http://www.yaochufa.com/index.php", $matches);
-//$regex->preg('/\bwww/',
-// "http://www.yaochufa.com/index.php", $matches);
-echo "host:" . $host = $matches[0] . "<pre>";
-var_dump($matches);
-//获取主机名称的后面两部分
-preg_match('/[^.]+\.[^.]+$/', $host, $matches);
-echo "domain name is: {$matches[0]}\n";
+$regex = new \Regex\Regexer();
+$array = array("Linux RedHat9.0", "Apache2.2.9", "MySQL5.0.51", "PHP5.2.6", "LAMP", "100");
+echo "<pre><h1> first : </h1><pre>";
+var_dump($regex->regexerArray($array));
+echo "<pre><h1> second : </h1><pre>";
+$row = "http://www.yaochufa.com/index.php";
+var_dump($regex->regexerHttp($row));
+echo "<pre><h1> third : </h1><pre>";
+$arr = "<tr><td><a href=\"http://qzone.qq.com\">QQ空间</a></td><td><a href=\"http://www.ganji.com\">赶 集 网</a></td><td>";
+var_dump($regex->takeInte($arr));
+echo "<pre><h1> fourth : </h1><pre>";
+$arrDelect = "这个文本中有<b>粗体</b>和<u>带有下划线</u>以及<i>斜体</i>还有<font color='red' size='7'>带有颜色和字体大小</font>的标记";
+var_dump($regex->deleteHtml($arrDelect));
